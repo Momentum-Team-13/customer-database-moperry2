c4aba2e7e7546059775c8b3bb8369cff96ab44e3
Michael Perry <moperrry@gmail.com>
2022-06-01 16:53:13 -0400

    Almost finished

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 const allCustomers = document.getElementById("directory");
 //Loop of each customer
+
+function capitalizeFirstLetter(word) {
+  console.log("capitalize name");
+  return word.charAt(0).toUpperCase() + word.slice(1);
+}
+
 for (let customer of customers) {
   console.log(`${customer.name.first} ${customer.name.last}`);
   let customerDiv = document.createElement("div");
@@ -14,7 +20,9 @@ for (let customer of customers) {
   //Name
 
   let nameEl = document.createElement("h3");
-  nameEl.innerText = `${customer.name.first} ${customer.name.last}`;
+  nameEl.innerText = `${capitalizeFirstLetter(
+    customer.name.first
+  )} ${capitalizeFirstLetter(customer.name.last)}`;
   customerDiv.appendChild(nameEl);
 
   //Email
@@ -24,8 +32,39 @@ for (let customer of customers) {
   customerDiv.appendChild(customerEmail);
 
   //Address
-
+  let addressEl = document.createElement("div");
+  addressEl.innerText = `${customer.location.street.number}
+    ${customer.location.street.name} ${customer.location.city},
+    ${nameToAbbr(customer.location.state)}
+    ${customer.location.postcode}`;
+  customerDiv.appendChild(addressEl);
   //DOB
+  //   let customerDOBP = document.createElement("p");
+  //   let dateOfBirth = moment(customer.dob.date).format("MMM D, YYYY");
+  //   console.log(dateOfBirth);
+  //   customerDOBP.innerText = `DOB: ${dateOfBirth}`;
+  //   customerDiv.appendChild(customerDOBP);
+
+  let dobElement = document.createElement("div");
+  let dobFormat = moment(customer.dob.date).format("MMM D, YYYY");
+  dobElement.classList.add("f5", "fw4", "black", "mt0");
+  dobElement.innerText = `DOB: ${dobFormat}`;
+  customerDiv.appendChild(dobElement);
+  //customer since
+  let customerSinceElement = document.createElement("div");
+  let registeredFormat = moment(customer.registered.date).format("MMM D, YYYY");
+  customerSinceElement.classList.add("f5", "fw4", "black", "mt0");
+  customerSinceElement.innerText = `Customer since: ${registeredFormat}`;
+  customerDiv.appendChild(customerSinceElement);
+
+  //   let dobEl = document.createElement("div");
+  //   dobEl.innerText = `${moment(customer.dob.date).format("MMM Do YYYY")}`;
+  //   customerDiv.appendChild(dobEl);
+
+  //   let dorEl = document.createElement("div");
+  //   dorEl.innerText = `${moment(customer.registered.date).format("MMM Do YYYY")}`;
+  //   customerDiv.appendChild(dorEl);
+
   allCustomers.appendChild(customerDiv);
 }
 //   let imageElement = document.createElement("img");
